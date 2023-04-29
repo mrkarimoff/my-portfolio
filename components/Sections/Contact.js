6b5a9762ad31125d2c7f67342a8847426276c8e4
mrkarimoff <mrkarimoff99@gmail.com>
2023-04-29 11:21:04 +0500

    changed request url

--- components/Sections/Contact.js
+++ components/Sections/Contact.js
@@ -20,7 +20,7 @@ function Contact() {
     e.preventDefault();
     try {
       const data = { name, email, message };
-      await axios.post("http://localhost:3000/api/email", data);
+      await axios.post("https://my-portfolio-mrkarimoff.vercel.app/api/email", data);
       toast.success("Message sent successfully!", {
         position: "top-right",
         autoClose: 2500,
