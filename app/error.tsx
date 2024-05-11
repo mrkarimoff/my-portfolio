195d6639052c73b123de80d86e00ef6cf21e7f7f
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-05-11 13:34:39 +0500

    add error pages

---
+++ app/error.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect } from 'react';
+import styles from '~/styles/error.module.css';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className={styles.errorContainer}>
+      <h2 className={styles.errorMessage}>Something went wrong! 🙃</h2>
+      <Image
+        className={styles.gif}
+        alt="error-gif"
+        height={400}
+        width={300}
+        src={'/error.gif'}
+      />
+      <div className={styles.btnWrapper}>
+        <button className={styles.btn} onClick={() => reset()}>
+          Try again
+        </button>
+        <button className={styles.btn}>
+          <Link href="/">Return Home</Link>
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '~/styles/error.module.css';
+
+export default function NotFound() {
+  return (
+    <div className={styles.errorContainer}>
+      <h2 className={styles.errorMessage}>404 | Page Not Found</h2>
+      <Image
+        className={styles.gif}
+        alt="error-gif"
+        height={400}
+        width={300}
+        src={'/error.gif'}
+      />
+      <button className={styles.btn}>
+        <Link href="/">Return Home</Link>
+      </button>
+    </div>
+  );
+}
